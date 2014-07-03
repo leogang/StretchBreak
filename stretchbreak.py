d3dd3df09906af9b38c3c19bc3e5faf6b7d98ec8
Cody Albert <animaltalk@animals-MacBook-Air.local>
2014-07-03 12:55:54 -0700

    Alerts user when to take a break from the computer
    Alerts user when to take a break from the computer. User can choose a
    specified amount of time and youtube video/song to open upon completion
    of the specified time.

---
+++ stretchbreak.py
@@ -0,0 +1,7 @@
+import time
+import webbrowser
+
+print "This program started on " + time.ctime()
+for i in range(0, 3):
+    time.sleep(60 * 45)
+    webbrowser.open("http://www.youtube.com/watch?v=balfsoIaIDs")
