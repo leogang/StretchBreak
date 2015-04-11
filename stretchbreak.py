3b93c09c7f9edfd8cf80653cc51743b96f1879d0
dreamingthemaze <leogang89@gmail.com>
2015-04-11 14:20:40 -0700

    Update stretchbreak.py

--- stretchbreak.py
+++ stretchbreak.py
@@ -4,4 +4,4 @@ import webbrowser
 print "This program started on " + time.ctime()
 for i in range(0, 3):
     time.sleep(60 * 45)
-    webbrowser.open("http://www.youtube.com/watch?v=balfsoIaIDs")
+    webbrowser.open("https://www.youtube.com/watch?v=iiZEto2j2GQ")
